b8cb78593d96eb8cc61467dcc74052d07cdbecb6
yang shao <yangshao@yangs-MacBook-Pro.local>
2017-09-17 17:29:52 -0400

    created class/static variable for ratings, and added some comments

--- movie.py
+++ movie.py
@@ -3,10 +3,12 @@ import webbrowser
 import media
 
 class Movie(media.Media):
+	#like static variable which is class level
 	VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 	def __init__(self, movie_title, movie_storyline, poster_image, tailer_youtube, rating):
+		#inheritance, title is from parent media
 		media.Media.__init__(self, movie_title)
 		self.storyline = movie_storyline
 		self.poster_image_url = poster_image
 		self.trailer_youtube_url = tailer_youtube
-		self.rating = rating
+		self.rating = rating if rating in self.VALID_RATINGS else 'N/A'
